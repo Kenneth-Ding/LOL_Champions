75b55d59940a5e4726dd0ca292ba6dcb205549c2
Starmy <mf99250@gmail.com>
2020-11-30 16:31:37 +0800

    Initial Commit

---
+++ LOL_Champions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LOL_Champions
+//
+//  Created by User19 on 2020/11/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LOL_Champions/LOL_ChampionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOL_ChampionsApp.swift
+//  LOL_Champions
+//
+//  Created by User19 on 2020/11/30.
+//
+
+import SwiftUI
+
+@main
+struct LOL_ChampionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
